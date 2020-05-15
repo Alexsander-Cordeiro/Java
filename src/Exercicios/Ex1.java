be26f6e492e74eaf0f023d7d6d9d1a91c7206570
Alexsander-Cordeiro <alexdex12@hotmail.com>
2020-05-15 13:48:42 -0300

    Exercicios java até o 7

---
+++ src/Exercicios/Ex1.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+
+public class Ex1 {
+	public static void main(String args[]) {
+		
+		int somaImpar=0;
+				
+		System.out.println("Os numeros impares são: ");
+		for(int i= 1;i <= 500;i++) {
+			
+				
+			if(i%2 != 0 && i%3 == 0) {
+				somaImpar = somaImpar + i;
+				System.out.println(i);
+				
+			}else {
+				
+			}
+			
+		}
+	
+		
+	}
+
+}
---
+++ src/Exercicios/Ex3.java
@@ -0,0 +1,24 @@
+package Exercicios;
+
+public class Ex3 {
+	public static void main(String args[]) {
+		
+		
+		
+		for(int i=233; i < 456;i++) {
+			if(i < 300) {
+				System.out.println(i);
+				
+			}
+			else if(i > 299 && i <401) {
+				i =  i + 2;
+				System.out.println(i);
+				
+			}else {
+				i = i + 4;
+				System.out.println(i);
+			}
+	}
+}
+	
+}
